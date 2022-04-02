68f3fb6f82cdf84c51eb4185d1b0001a6d5e94e1
apple <melvinfernando175@gmail.com>
2022-04-02 21:13:01 +0530

    make it better

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/User.js");
 const ErrorResponse = require("../utils/errorResponse");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
