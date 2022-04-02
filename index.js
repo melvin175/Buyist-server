4ecb837719360c279d81d1c16de593e06b7c92a8
apple <melvinfernando175@gmail.com>
2022-04-03 00:40:32 +0530

    make it better

--- index.js
+++ index.js
@@ -32,12 +32,6 @@ app.get("/", (req, res) => {
   res.send("Hey there, welcome to the api!");
 });
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || "0.0.0.0";
-server.listen(server_port, server_host, function () {
-  console.log("Listening on port %d", server_port);
-});
-
 process.on("unhandledRejection", (err) => {
   console.log(`Logged Error: ${err}`);
   server.close(() => process.exit(1));
